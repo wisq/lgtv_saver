76f12916ab0ef82e431c9d3e8e08c863dc2e61b1
Adrian Irving-Beer <wisq@wisq.net>
2020-11-08 04:56:35 -0500

    Switch to tracking milliseconds.

--- lib/lgtv_saver/watcher.ex
+++ lib/lgtv_saver/watcher.ex
@@ -21,12 +21,12 @@ defmodule LgtvSaver.Watcher do
   end
 
   defp current_time() do
-    DateTime.utc_now() |> DateTime.to_unix()
+    DateTime.utc_now() |> DateTime.to_unix(:millisecond)
   end
 
   @impl true
   def init({tv, input, options}) do
-    idle_time = Map.get(options, :idle_time, @default_idle_time)
+    idle_time = Map.get(options, :idle_time, @default_idle_time) * 1000
     ip = Map.get(options, :bind, {0, 0, 0, 0})
     port = Map.fetch!(options, :port)
 
@@ -45,9 +45,9 @@ defmodule LgtvSaver.Watcher do
   @impl true
   def handle_info({:udp, socket, _address, _port, data}, %State{socket: socket} = state) do
     case :string.chomp(data) |> Integer.parse() do
-      {secs, ""} ->
-        Logger.debug("#{state.input}: Idle for #{secs} seconds")
-        {:noreply, %State{state | last_active: current_time() - secs}, {:continue, :activity}}
+      {msecs, ""} ->
+        Logger.debug("#{state.input}: Idle for #{msecs} ms")
+        {:noreply, %State{state | last_active: current_time() - msecs}, {:continue, :activity}}
 
       _ ->
         Logger.debug("#{state.input}: Unexpected data: #{inspect(data)}")
@@ -59,21 +59,27 @@ defmodule LgtvSaver.Watcher do
   def handle_info(:timeout, state) do
     Logger.debug("#{state.input}: Activity timeout")
     TV.inactive(state.tv, state.input)
-    {:noreply, state, state.idle_time * 1000}
+    {:noreply, state, state.idle_time}
   end
 
   @impl true
   def handle_continue(:activity, state) do
-    secs = state.last_active + state.idle_time - current_time()
+    msecs = state.last_active + state.idle_time - current_time()
 
-    if secs > 0 do
-      Logger.debug("#{state.input}: Activity timeout in #{inspect(secs)} seconds")
-      TV.active(state.tv, state.input)
-      {:noreply, state, Kernel.max(0, secs * 1000)}
-    else
-      Logger.debug("#{state.input}: Activity timeout #{inspect(0 - secs)} seconds ago")
-      TV.inactive(state.tv, state.input)
-      {:noreply, state, state.idle_time * 1000}
+    cond do
+      msecs > 1000 ->
+        Logger.debug("#{state.input}: Activity timeout in #{inspect(msecs)} ms")
+        TV.active(state.tv, state.input)
+        {:noreply, state, msecs}
+
+      msecs > 0 ->
+        Logger.debug("#{state.input}: Activity timeout in less than a second")
+        {:noreply, state, msecs}
+
+      true ->
+        Logger.debug("#{state.input}: Activity timeout #{inspect(0 - msecs)} ms ago")
+        TV.inactive(state.tv, state.input)
+        {:noreply, state, state.idle_time * 1000}
     end
   end
 end
